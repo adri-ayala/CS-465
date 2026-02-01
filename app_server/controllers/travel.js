f9541232e3e308c7dc6162bd2fb1c90b35afaaa5
adri-ayala <Adrienne.ayala.c@gmail.com>
2026-02-01 12:37:21 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,9 @@
+var fs = require('fs');
+var tripd = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+
 const travel = (req,res) => {
-    res.render('travel',{ title: "Travel Getaways"});
+    res.render('travel',{ title: 'Travel Getaways', trips});
 };
 module.exports = {
     travel
